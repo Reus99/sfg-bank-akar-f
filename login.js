06f8e716a80ba37eee88fb04205d7e41aaab5fc7
Reus99 <berkoncu99@gmail.com>
2023-01-28 00:28:50 +0300

    dedede

---
+++ login.js
@@ -0,0 +1,48 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+const kimlikNo = document.getElementById("tc_kimlik_no");
+const adSoyad = document.getElementById("adSoyad");
+const email = document.getElementById("email");
+const telNo= document.getElementById("telNo");
+const adres = document.getElementById("adres");
+const sifre = document.getElementById("sifre");
+const erkek = document.getElementById("erkek");
+const kadin = document.getElementById("kadin");
+const hesapOlustur = document.getElementById("hesapOlustur");
+
+hesapOlustur.addEventListener("click",xhr.onreadystatechange);
+
+let xhr = new XMLHttpRequest();
+xhr.open("POST", "https://reqbin.com/echo/post/json%22");
+xhr.setRequestHeader("Accept", "application/json");
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    console.log(xhr.status);
+    console.log(xhr.responseText);
+  }};
+
+let data = {
+  "kimlikNo": kimlikNo,
+  "adSoyad": adSoyad,
+  "email": email,
+  "telNo": telNo,
+  "adres": adres,
+  "sifre": sifre,
+  "cinsiyetErkek":erkek,
+  "cinsiyetKadin":kadin
+};
+
+xhr.send(data);
+
