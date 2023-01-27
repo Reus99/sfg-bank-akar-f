107ad5f30debf039e397f128fb1a3c0ee07a33f5
Reus99 <berkoncu99@gmail.com>
2023-01-28 00:35:34 +0300

    test deneme

--- login.js
+++ login.js
@@ -30,6 +30,7 @@ xhr.setRequestHeader("Content-Type", "application/json");
 xhr.onreadystatechange = function () {
   if (xhr.readyState === 4) {
     console.log(xhr.status);
+    //deneme  asd asd a
     console.log(xhr.responseText);
   }};
 
