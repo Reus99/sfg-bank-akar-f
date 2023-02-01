294b8f3c893d8d170f8b814b17d94549c09d8d44
Reus99 <berkoncu99@gmail.com>
2023-02-02 00:34:25 +0300

    close#2

--- login.js
+++ login.js
@@ -11,6 +11,9 @@ signInButton.addEventListener("click", () => {
 });
 
 const kimlikNo = document.getElementById("tc_kimlik_no");
+const girisTc = document.getElementById("girisTc");
+const girisSifre = document.getElementById("girisSifre");
+const girisBtn = document.getElementById("giris");
 const adSoyad = document.getElementById("adSoyad");
 const email = document.getElementById("email");
 const telNo = document.getElementById("telNo");
@@ -20,6 +23,8 @@ const erkek = document.getElementById("erkek");
 const kadin = document.getElementById("kadin");
 const hesapOlustur = document.getElementById("hesapOlustur");
 let cinsiyet = null;
+
+
 
 hesapOlustur.addEventListener("click", function (event) {
   event.preventDefault();
@@ -75,7 +80,19 @@ xhr.send(data);*/
 
   fetch("http://localhost:9999/giris/kimlik", requestOptions)
     .then((response) => response.text())
-    .then((result) => console.log(result))
+    .then( result => {
+      var jsonData= JSON.parse(result);
+      var adiSoyadi = jsonData.adiSoyadi;
+        if(jsonData.ekleControl){
+          console.log("Kayıt Başarılı");
+          Swal.fire({
+            icon: 'success',
+            title: 'BANKAMIZA HOŞ GELDİNİZ'+" "+adSoyad.value,
+            showConfirmButton: false,
+            timer: 2000
+          })
+        };
+    } )
     .catch((error) => console.log("error", error));
 });
 
@@ -87,3 +104,57 @@ function getcinsiyet() {
   }
   return cinsiyet;
 }
+
+
+girisBtn.addEventListener("click", function (eventGiris) {
+  eventGiris.preventDefault();
+  /*
+let xhr = new XMLHttpRequest();
+xhr.open("POST", "localhost:9999/giris/kullaniciekle.html");
+xhr.setRequestHeader("Accept", "application/json");
+xhr.setRequestHeader("Content-Type", "application/json");
+
+
+xhr.onreadystatechange = function (e) {
+  e.preventDefault
+  if (xhr.readyState === 4) {
+    console.log(xhr.status);
+    console.log(xhr.responseText);
+  }};
+
+xhr.send(data);*/
+
+  let data1 = {
+    "kullaniciKodu": girisTc.value,
+    "sifre": girisSifre.value,
+  };
+
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify(data1);
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch("BANKSISTEM/login/auth", requestOptions)
+    .then((response) => response.text())
+    .then( result => {
+      var jsonData= JSON.parse(result);
+        if(jsonData.ekleControl){
+          console.log("Giriş Başarılı");
+          Swal.fire({
+            icon: 'success',
+            title: 'BANKAMIZA TEKRAR HOŞ GELDİNİZ'+" "+adSoyad.value,
+            showConfirmButton: false,
+            timer: 2000
+          })
+          window.location.href = "smsregister.html"
+        };
+    } )
+    .catch((error) => console.log("error", error));
+});
